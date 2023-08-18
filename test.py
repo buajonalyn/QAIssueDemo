8348a3ac7cd2c27fd2e7182921a51d88edbbbbb1
unknown <jonalyn_bua@solenergy.com.ph>
2023-08-18 08:51:36 +0800

    Updated comment

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from wallet import Wallet
 
+#test
 def test_getbalance():
     obj = Wallet(0)
     obj.set_balance(20)
@@ -14,4 +15,3 @@ def test_setbalance():
     obj = Wallet(0)
     obj.set_balance(40)
     assert obj.get_balance() == 40
-    
